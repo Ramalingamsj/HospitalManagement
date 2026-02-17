af70cb3f1bb8b384291c434b02425898970498dc
Ramalingamsj <ramalingamsj21@gmail.com>
2026-02-17 21:06:44 +0530

    nav add

---
+++ HospitalManagement/HospitalManagement/ViewModels/LabPendingVM.cs
@@ -0,0 +1,10 @@
+﻿namespace HospitalManagement.ViewModels
+{
+    public class LabPendingVM
+    {
+        public int PatientLabTestId { get; set; }
+        public string PatientName { get; set; }
+        public string TestName { get; set; }
+        public int StatusId { get; set; }
+    }
+}
